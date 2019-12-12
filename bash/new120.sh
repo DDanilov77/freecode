b09884ca82919ec784bb49620a3196ee2998e3c2
DDanilov77 <ddanilov77@mail.ru>
2019-12-12 11:19:28 +0300

    Update new120.sh
    ready for test

--- bash/new120.sh
+++ bash/new120.sh
@@ -3,12 +3,25 @@
 #alias new120='~/script/new120.sh'
 #run this script for send key to vagrant box
 
-ip12=('192.168.20.120' '192.168.20.121' '192.168.20.122')
-for value in ${ip12[@]};   do    
-    echo "Remove ssh-key"
-    ssh-keygen -f ~/.ssh/known_hosts -R $value
-    echo "Send ssh-key to server"
-    ssh-copy-id -i ~/.ssh/$USER.pub root@$value
-    echo $value
-done
+#!/bin/bash
 
+#alias new120='~/script/new120.sh'
+
+ip12=('192.168.11.120' '192.168.11.121' '192.168.11.122' '192.168.11.123' '192.168.11.124' '192.168.11.125')
+FROMPATH='~/Downloads/2server/*'
+
+#run this script for send key to vagrant box
+if [ ! -f ~/.ssh/$USER.pub ]
+    then
+    #create key
+    ssh-keygen -t rsa -b 2048 -f ~/.ssh/$USER
+fi
+    for value in ${ip12[@]};   do
+    echo "Remove ssh-key from known_hosts"
+    ssh-keygen -f ~/.ssh/known_hosts -R $value
+    echo "Send ssh-key to servers"
+    ssh-copy-id -i ~/.ssh/$USER.pub $USER@$value
+    #echo $value
+    scp -vf $FROMPATH $USER@$value:/home/$USER/
+    done
+echo "Servers ready for testing"
